c62b2f538c73dc967f94d6450df34366a45f8d4a
kasmr <alikkras@gmail.com>
2020-05-14 00:15:19 +0300

    types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import Header from './components/Header';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./store/configureStore";
+import AppRouter from "./router";
 
 const App: React.FC = () => {
-  return <Header />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 };
 
 export default App;
